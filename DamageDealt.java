71e99bc81ff28602dc3d0ecf75d32cc2d2257e98
Rachel. M <70598758+s11715224@users.noreply.github.com>
2021-10-22 08:52:04 -0400

    v.0.2

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,11 +1,11 @@
-// <robin/rachel macdonald>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <9:23>, v0.1
+// <robin/rachel macdonald>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/22/2021>, <8:50>, v0.2
 
 import java.util.Scanner;
 
 public class DamageDealt   {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        
+        System.out.println("Enter the name of your wepond:");
 
     }
     
