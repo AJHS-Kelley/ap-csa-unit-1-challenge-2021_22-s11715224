69139b4e9059a427695618c52583044a5b2fdfef
Jordan Johnson <73534211+ovoxay@users.noreply.github.com>
2021-10-22 09:09:27 -0400

    Update DamageDealt.java

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,5 +1,5 @@
 // <robin/rachel macdonald>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/22/2021>, <8:50>, v0.2
-
+// Peer Debugging Jordan Johnson
 import java.util.Scanner;
 
 public class DamageDealt   {
