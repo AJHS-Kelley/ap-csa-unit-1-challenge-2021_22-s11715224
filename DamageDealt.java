0a59347f3eeeecd8c602e2332c1111237c121251
Venti <70521040+s11029568@users.noreply.github.com>
2021-10-22 09:08:00 -0400

    Peer Review
    Spelling Error on line 8

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,11 +1,11 @@
 // <robin/rachel macdonald>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/22/2021>, <8:50>, v0.2
-
+// Peer Review, Maxx Secrest
 import java.util.Scanner;
 
 public class DamageDealt   {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        System.out.println("Enter the name of your wepond:");
+        System.out.println("Enter the name of your wepond:"); //Spelling error
 
     }
     
