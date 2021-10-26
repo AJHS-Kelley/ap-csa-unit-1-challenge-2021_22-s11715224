86807d968cb2fac32a5257c12fd4078d3a487e2b
Rachel. M <70598758+s11715224@users.noreply.github.com>
2021-10-26 09:36:27 -0400

    v.0.8
    numverAttacks

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,4 +1,4 @@
-// <robin/rachel macdonald>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/26/2021>, <9:21>, v0.7
+// <robin/rachel macdonald>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/26/2021>, <9:39>, v0.8
 // Peer Debugging Jordan Johnson
 import java.util.Scanner;
 
@@ -7,14 +7,16 @@ public class DamageDealt   {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("What is the name of the monster?");
         String monsterName = myScanner.nextLine();
-        System.out.print("hello, ");
-        System.out.println(monsterName);
+        System.out.println("hello" + monsterName);
         System.out.println("Enter the name of your wepond:");
         String weaponName = myScanner.nextLine();
         System.out.println("\n");
         System.out.println("Please enter the damage value of the weapon");
         double weaponDamage = myScanner.nextDouble();
         System.out.println("\n");
+        System.out.println("How many attacks do you want to make?");
+        var numberAttacks = myScanner.nextLine();
+
 
     }
 
